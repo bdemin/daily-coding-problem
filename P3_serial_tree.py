7ef24f7a3c94b9c1c283cc5af04d3035b47e9e25
bdemin <42113340+bdemin@users.noreply.github.com>
2019-06-27 20:07:25 +0300

    P3 - Solved Serialize

--- P3_serial_tree.py
+++ P3_serial_tree.py
@@ -8,16 +8,28 @@ class Node:
         self.left = left
         self.right = right
 
+    def serialize(self, root, output = []):
+        if root == None:
+            output.append(None)
+            return None
 
-def serialize(node):
-    pass
+        output.append(root.val)
+        self.serialize(root.left)
+        self.serialize(root.right)
+        return output
 
-def deserialize(node):
-    pass
 
+def deserialize(_input):
+    if _input == None:
+        return None
+    node = Node(_input[0])
+    _input.pop(0)
+    node.left = deserialize(_input)
+    node.right = deserialize(_input)
 
 
 # Test driver:
 node = Node('root', Node('left', Node('left.left')), Node('right'))
-print(node)
+test = node.serialize(node)
+deserialize(test)
 # assert deserialize(serialize(node)).left.left.val == 'left.left'
