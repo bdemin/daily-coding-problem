ef6d5a61d667b86cf6d711148729807e4e89fdc1
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-14 21:55:38 +0300

    Solved P21_num_rooms

--- P21_num_rooms.py
+++ P21_num_rooms.py
@@ -6,29 +6,29 @@
 # should return 2.
 
 
-# Brute force
-def is_between(num, pair):
-    if num >= pair[0] and num <= pair[1]:
-        return True
-    return False
-
-
-def find_num_rooms(arr):
+def better_find_num_rooms(arr):
+    sortedStart = sorted([num[0] for num in arr])
+    sortedEnd = sorted([num[1] for num in arr])
+    
     numRooms = 0
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if is_between(arr[i][0], arr[j]):
-                numRooms += 1
-            if is_between(arr[i][1], arr[j]):
-                numRooms += 1
-    return numRooms
+    startedTime = 0
+    endedTime = 0
+    maxRooms = 0
+    while startedTime < len(arr) and endedTime < len(arr):
+        if sortedStart[startedTime] < sortedEnd[endedTime]:
+            numRooms += 1
+            startedTime += 1
+        else:
+            endedTime += 1
+            numRooms -= 1
+        maxRooms = max(numRooms, maxRooms)
+    return maxRooms
 
 
 # Driver code
 arr = [(30, 75), (0, 50), (60, 150)]
-result = find_num_rooms(arr)
+result = better_find_num_rooms(arr)
 print(result)
 arr = [(0, 50), (40, 50), (30, 50), (60, 70)]
-result = find_num_rooms(arr)
+result = better_find_num_rooms(arr)
 print(result)
-
