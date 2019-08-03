e3ec4c184dfebb91d81049774f1f300ec8f9a7d9
bdemin <42113340+bdemin@users.noreply.github.com>
2019-08-03 16:35:09 +0300

    Update P40_integer_occurrence.py

--- P40_integer_occurrence.py
+++ P40_integer_occurrence.py
@@ -9,14 +9,40 @@
 
 
 def find_single_occurance(arr):
-    arr_sum = sum(arr)
-    arr_len = len(arr)
-    # num_divisions = (arr_len - 1) / 3
-
-    for i in range(arr_len):
-        if (arr_sum - arr[i]) % 3 == 0:
-            return arr[i]
-    return False
+    n = len(arr)
+    ones = 0
+    twos = 0
+      
+    for i in range(n): 
+        # one & arr[i]" gives the bits that 
+        # are there in both 'ones' and new 
+        # element from arr[]. We add these 
+        # bits to 'twos' using bitwise OR 
+        twos = twos | (ones & arr[i]) 
+          
+        # one & arr[i]" gives the bits that 
+        # are there in both 'ones' and new 
+        # element from arr[]. We add these 
+        # bits to 'twos' using bitwise OR 
+        ones = ones ^ arr[i] 
+          
+        # The common bits are those bits  
+        # which appear third time. So these 
+        # bits should not be there in both  
+        # 'ones' and 'twos'. common_bit_mask 
+        # contains all these bits as 0, so 
+        # that the bits can be removed from 
+        # 'ones' and 'twos' 
+        common_bit_mask = ~(ones & twos) 
+          
+        # Remove common bits (the bits that  
+        # appear third time) from 'ones' 
+        ones &= common_bit_mask 
+          
+        # Remove common bits (the bits that 
+        # appear third time) from 'twos' 
+        twos &= common_bit_mask 
+    return ones 
 
 
 # Driver code
